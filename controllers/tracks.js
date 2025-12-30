bac7915b166166972dcc975ac40df96e1d6e2df0
Ali-Albanna1 <ali.albanna015@gmail.com>
2025-12-30 03:32:35 +0300

    finish delete and edit of track

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -7,7 +7,7 @@ const Track = require('../models/track')
 // init router
 const router = express.Router()
 
-//post + /pets/
+//post + /tracks/
 router.post('/', async (req,res)=> {
 
     try {
@@ -60,6 +60,62 @@ router.get('/:id', async (req,res) => {
     }
 })
 
+// DEL + /tracks/123
+router.delete('/:id', async (req, res)=>{
+
+    try{
+      // get the id from params
+     
+        const {id}= req.params
+      
+       const track = await Track.findByIdAndDelete(id)
+
+       
+       if(!Track){
+
+         res.status(404).json({error: 'track not found'})
+       } else{
+        // use 204 if u dont want to send new thing
+        res.status(200).json({track})
+       }
+
+       
+
+  }
+    catch(error){
+
+        console.log(error)
+
+        res.status(500).json({error: "failed to delete track"})
+    }
+
+
+
+})
+
+//PUT + /track/123
+router.put('/:id', async (req,res)=>{
+    try{
+
+        
+        const {id} =req.params
+        const track = await Track.findByIdAndUpdate(id, req.body, {new:true})
+
+
+        if(!track){
+            res.status(404).json({error: 'track Not found'})
+
+        }else{
+               res.status(200).json({track})
+        }
+    }
+    catch(error){
+        console.log(error)
+        res.status(500).json({error: 'failed to update track'})
+    }
+
+
+})
 
 
 
